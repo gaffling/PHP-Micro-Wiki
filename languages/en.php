f5f64bb0e201ff5a4d09d7a8b00a1d99d198cd3d
I. Gaffling <10500034+gaffling@users.noreply.github.com>
2020-01-07 11:47:57 +0100

    Create en.php

---
+++ languages/en.php
@@ -0,0 +1,66 @@
+<?PHP
+
+/* Please note - if you are translating this file,
+ * that any quotes need a backslash e.g. \"
+ * to escape this extra quotes in the string.
+ * Any variables in this file need to stay in the same format.
+ * ONLY insert translation text right from the = between the two double ("") quotes.
+ */
+
+$i18n = array(
+	'langflag'		=> "gb",
+	'langname'		=> "English",
+	'charset'		=> "utf-8",
+	'langdir'		=> "ltr", // ltr OR rtl
+	'version'		=> "1.0",
+);
+$i18n['Backend'] = "Backend";
+$i18n['Enter'] = "Enter";
+$i18n['Edit Wiki Page'] = "Edit Wiki Page";
+$i18n['Folder'] = "Folder";
+$i18n['File'] = "File";
+$i18n['Revisions'] = "Revisions";
+$i18n['SAVE FILE'] = "SAVE FILE";
+$i18n['SAVE NEW FILE'] = "SAVE NEW FILE";
+$i18n['DELETE FILE'] = "DELETE FILE";
+$i18n['DELETE FOLDER'] = "DELETE FOLDER";
+$i18n['DOWNLOAD DATA AS ZIP'] = "DOWNLOAD DATA AS ZIP";
+$i18n['BACK TO FRONTEND'] = "BACK TO FRONTEND";
+$i18n['ROLLBACK'] = "ROLLBACK";
+$i18n['HELP'] = "'SAVE FILE' HELP
+
+New Dirname & new Filename = 
+Make Dir & Save as New File
+
+New Dirname & same Filename = 
+Make Dir, Move & Save File
+
+Same Dirname & new Filename = 
+Rename File & Save File
+
+Same Dirname & same Filename = 
+Save File
+";
+
+$i18n['Are you sure'] = "Are you sure";
+$i18n['you want to delete the following file'] = "to delete the following file";
+$i18n['you want to delete the following directory'] = "to delete the following directory";
+$i18n['HINT!'] = "HINT!";
+$i18n['Only if it\'s empty! We will check first!'] = "Only if it's empty! We will check!";
+$i18n['Dir made and renamed file saved!'] = "Dir made and renamed file saved!";
+$i18n['Dir made and new file saved!'] = "Dir made and new file saved!";
+$i18n['ERROR while try to make dir or save new file!'] = "ERROR while try to make dir or save new file!";
+$i18n['Dir made, file moved and new file saved!'] = "Dir made, file moved and new file saved!";
+$i18n['New file saved!'] = "New file saved!";
+$i18n['ERROR while trying to save new file!'] = "ERROR while trying to save new file!";
+$i18n['File renamed and saved!'] = "File renamed and saved!";
+$i18n['New file saved!'] = "New file saved!";
+$i18n['ERROR, maybe filename exists already in this Dir?'] = "ERROR, maybe filename exists already in this Dir?";
+$i18n['File saved!'] = "File saved!";
+$i18n['File deleted!'] = "File deleted!";
+$i18n['ERROR while trying to delete file!'] = "ERROR while trying to delete file!";
+$i18n['Directory deleted!'] = "Directory deleted!";
+$i18n['ERROR while trying to delete directory, maybe not empty?'] = "ERROR while trying to delete directory, maybe not empty?";
+$i18n['ERROR homepage can not be deleted!'] = "ERROR homepage can not be deleted!";
+
+/* EOF */

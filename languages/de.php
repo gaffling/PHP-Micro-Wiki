30d45e2a93a875b27d117274aa8bf72da93b2659
I. Gaffling <10500034+gaffling@users.noreply.github.com>
2020-01-07 11:47:33 +0100

    Update de.php

--- languages/de.php
+++ languages/de.php
@@ -1,1 +1,66 @@
+<?PHP
 
+/* Please note - if you are translating this file,
+ * that any quotes need a backslash e.g. \"
+ * to escape this extra quotes in the string.
+ * Any variables in this file need to stay in the same format.
+ * ONLY insert translation text right from the = between the two double ("") quotes.
+ */
+
+$i18n = array(
+	'langflag'		=> "de",
+	'langname'		=> "German",
+	'charset'		=> "utf-8",
+	'langdir'		=> "ltr", // ltr OR rtl
+	'version'		=> "1.0",
+);
+$i18n['Backend'] = "Admin";
+$i18n['Enter'] = "Login";
+$i18n['Edit Wiki Page'] = "Seite bearbeiten";
+$i18n['Folder'] = "Ordner";
+$i18n['File'] = "Datei";
+$i18n['Revisions'] = "Revisionen";
+$i18n['SAVE FILE'] = "DATEI SPEICHERN";
+$i18n['SAVE NEW FILE'] = "NEUE DATEI SPEICHERN";
+$i18n['DELETE FILE'] = "DATEI LÖSCHEN";
+$i18n['DELETE FOLDER'] = "ORDNER LÖSCHEN";
+$i18n['DOWNLOAD DATA AS ZIP'] = "DATEN ALS ZIP ARCHIV";
+$i18n['BACK TO FRONTEND'] = "ZURÜCK ZUR STARTSEITE";
+$i18n['ROLLBACK'] = "ROLLBACK";
+$i18n['HELP'] = "'DATEI SPEICHERN' HILFE
+
+Ordner & Dateiname neu = 
+beides erstellen & Datei speichern
+
+Nur Ordner neu = 
+Ordner erstellen,
+Datei verschieben & speichern
+
+Nur Dateiname neu = 
+Datei umbenennen & speichern
+
+Ordner & Dateiname alt = 
+Datei speichern
+";
+$i18n['Are you sure'] = "Sind Sie sicher";
+$i18n['you want to delete the following file'] = "folgende Datei zu löschen";
+$i18n['you want to delete the following directory'] = "folgenden Ordner zu löschen";
+$i18n['HINT!'] = "TIPP!";
+$i18n['Only if it\'s empty! We will check first!'] = "Nur, wenn leer! Wir prüfen das!";
+$i18n['Dir made and renamed file saved!'] = "Ordner erstellt und umbenannte Datei gespeichert!";
+$i18n['Dir made and new file saved!'] = "Ordner erstellt und neue Datei gespeichert!";
+$i18n['ERROR while try to make dir or save new file!'] = "FEHLER beim Versuch, Ordner zu erstellen oder Daten zu speichern!";
+$i18n['Dir made, file moved and new file saved!'] = "Ordner erstellt, Datei verschoben und neue Daten gespeichert!";
+$i18n['New file saved!'] = "Neue Datei gespeichert!";
+$i18n['ERROR while trying to save new file!'] = "FEHLER beim Versuch, die neue Datei zu speichern!";
+$i18n['File renamed and saved!'] = "Datei umbenannt und gespeichert!";
+$i18n['New file saved!'] = "Neue Datei gespeichert!";
+$i18n['ERROR, maybe filename exists already in this Dir?'] = "ERROR, maybe filename exists already in this Dir?";
+$i18n['File saved!'] = "Datei gespeichert!";
+$i18n['File deleted!'] = "Datei gelöscht!";
+$i18n['ERROR while trying to delete file!'] = "FEHLER beim Versuch, die Datei zu löschen!";
+$i18n['Directory deleted!'] = "Ordner gelöscht!";
+$i18n['ERROR while trying to delete directory, maybe not empty?'] = "FEHLER beim Versuch, den Ordner zu löschen, vielleicht nicht leer?";
+$i18n['ERROR homepage can not be deleted!'] = "FEHLER Startseite kann nicht gelöscht werden!";
+
+/* EOF */
